3754d0f079c662b51290fecff9fe0cd040881376
Bartosz S <stvhh@Mac.lan>
2024-12-16 13:13:22 +0100

    Add basic main seperation

--- src/game.go
+++ src/game.go
@@ -1,3 +1,29 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	ScreenWidth  = 1280
+	ScreenHeight = 720
+	ScreenSize   = ScreenWidth * ScreenHeight
+)
+
 type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %0.1f", ebiten.ActualFPS()), 100, 1)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %0.1f", ebiten.ActualTPS()), 200, 1)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
